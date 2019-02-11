79a445ddd286e97a4b306012adeab8154d629684
planc509 <planc509@cs.washington.edu>
2019-02-11 14:09:26 -0800

    add racecar bashrc thingy with aliases to run darknet and stuff

---
+++ setup/racecar_bashrc.sh
@@ -0,0 +1,4 @@
+alias darknet='DISPLAY=:0 roslaunch darknet_ros darknet_ros.launch'
+alias person_follower='~/catkin_ws/src/person_follower/src/person_follower.py'
+alias teleop='roslaunch racecar teleop.launch'
+alias camera='roslaunch realsense2_camera rs_camera.launch'
